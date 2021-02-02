91df8a34ebd2ff4d48f4d7169756ecbc28f3ecbc
Luiza Neves <luiza.nbs@gmail.com>
2021-02-01 23:06:51 -0300

    Teste de commit

---
+++ Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato
+{
+    class Estudante
+    {
+        public string Nome;
+        public string Email;
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Estudante[] estudante = new Estudante[10];
+
+            for (i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                estudante[quarto] = new Estudante(nome, email);
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos alugados: ");
+
+            for (i = 0; i < 10; i++)
+            {
+                if (estudante[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudante[i] );
+                }
+            }
+        }
+    }
+}
